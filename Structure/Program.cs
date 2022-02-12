585fa513259bfc2016fa9a65cbd4f8e90dada67d
aidancer55 <bluebonnets77@gmail.com>
2022-02-12 12:32:30 +0300

    Добавьте файлы проекта.

---
+++ Structure/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Str str = new Str();
+            str.k = Convert.ToInt32(Console.ReadLine());
+            str.b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("0 = k * x + b");
+            Console.WriteLine("x = (0 - b) / k");
+            str.Root();
+            Console.ReadKey();
+        }
+        struct Str
+        {
+            public double x;
+            public double k;
+            public double b;
+            public void Root()
+            {
+                x = (0 - b) / k;
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
